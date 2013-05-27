910049a195c0e888d4e7aa1755f8d2919859a1a3
William Blanchette <wb@collectivecognition.com>
2013-05-27 14:35:05 -0600

    Added HSL/A functionalaity, rewrote large portions of the code to make things tidier, added some tests to cover new functionality

--- README.md
+++ README.md
@@ -6,15 +6,27 @@ Basic class to convert colour values in various forms to a generic object with s
 Currently handles:
 - Hex strings: #fff, abc123
 - RGB and RGBA values: rgba(0, 255, 0, 0.5)
+- HSL and HSLA values: hsla(30, 10%, 50%, 1.0)
 
-Support for named colours and HSL values planned, as well as utility methods to manipulate colors.
+Instantiation:
+==============
+	
+The **Colour** constructor creates a colour object from the specified string (hex, RGB, RGBA, HSL or HSLA):
 
-Example:
-========
-
-    console.log((new Color("rgb(0, 10%  , 255)")).rgba);
-    **{r: 0, g: 25, b: 255, a: 255}**
+    var colour = new Colour("#fff");
     
+You can modify the HSL, RGB and alpha values after instantiation using a chainable API:
+
+    colour.r(255).l(0.5);
+    
+Or retrieve values by using the same functions with no parameter:
+
+    console.log("Hue: " + colour.h());
+    
+Colours can be converted to usable strings using the **stringify** method, where **hex** can be any of **hex, rgb, rgba, hsl and hsla**:
+
+    colour.stringify("hex");
+        
 Tests:
 ======
 
--- tests.js
+++ tests.js
@@ -1,7 +1,30 @@
-var assert = function(test, description){console.log("[" + (test ? "PASS" : "FAIL") + "] " + description);}
+var assert = function(test, description){ console.log("[" + (test ? "PASS" : "FAIL") + "] " + description); };
 
+var util = require("util"); // FIXME
 require("./colour");
 
-assert((new Colour("#fff")).rgba.r === 255, "Shortened hex colour with preceding hash");
-assert((new Colour("aBC123")).rgba.r === 171, "Mixed-case hex colour with no hash");
-assert((new Colour("rgb(0, 10%  , 255)")).rgba.g === 25, "RGB with percent value and inline whitespace");
+var colour;
+
+colour = new Colour("#ff00aa");
+assert(colour.stringify("hex") === "ff00aa", "Hex parsing");
+
+colour = new Colour("rgb(255, 128, 0)");
+assert(colour.stringify("rgb") == "rgb(255, 128, 0)", "RGB parsing");
+
+colour = new Colour("rgba(255, 128, 0, 1.5)");
+assert(colour.stringify("rgba") == "rgba(255, 128, 0, 1)", "RGBA parsing");
+
+colour = new Colour("hsl(360, 25%, 50%)");
+assert(colour.stringify("hsl") === "hsl(360, 25%, 50%)", "HSL parsing");
+
+colour = new Colour("hsla(360, 25%, 50%, .5)");
+assert(colour.stringify("hsla") === "hsla(360, 25%, 50%, 0.5)", "HSLA parsing");
+
+colour = new Colour();
+assert(colour.r(255).g(128).b(64).a(.5).stringify("rgba") == "rgba(255, 128, 64, 0.5)", "Modifying RGB values");
+
+colour = new Colour("rgb(256, 128, 0)");
+assert(colour.stringify("hsla") === "hsla(30, 100%, 100%, 1)", "Converting from RGB to HSLA");
+
+colour = new Colour("hsl(30, 100%, 100%)");
+assert(colour.stringify("rgba") === "rgba(256, 128, 0, 1)", "Converting from HSL to RGBA");
